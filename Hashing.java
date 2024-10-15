374475d82e8e1f7b46eb397a2cf85ffa89e5d22e
Alakuntashiva02 <146987864+Alakuntashiva02@users.noreply.github.com>
2024-10-15 14:16:52 +0530

    Add files via upload
    created a hashset by using  built-in packages and added,removed the elements and checks if contains the elements

---
+++ Hashing.java
@@ -0,0 +1,37 @@
+import java.util.HashSet;
+import java.util.Iterator;
+
+
+
+public class Hashing {
+    public static void main(String args[]){
+        HashSet<Integer> set=new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(1);
+        System.out.println(set);
+
+        set.remove(1);
+        System.out.println(set);
+
+        if(set.contains(1)){
+            System.out.println("is available");
+        }
+        if(!set.contains(1)){
+            System.out.println("not available");
+        }
+        set.add(0);
+        set.add(4);
+        Iterator it=set.iterator();
+
+while(it.hasNext()){
+   System.out.println(it.next() + " ");
+
+}
+
+    }
+}
+
+
+       
